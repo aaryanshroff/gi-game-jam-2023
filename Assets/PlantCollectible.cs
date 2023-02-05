3c0884954c5135b854fae0d7665b6fdfcdcfe842
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-02-05 13:02:00 -0500

    collect trees

--- Assets/PlantCollectible.cs
+++ Assets/PlantCollectible.cs
@@ -7,11 +7,25 @@ using TMPro;
 public class PlantCollectible : MonoBehaviour
 {
     public TextMeshProUGUI plantText;
+    public bool isInTrigger = false;
     void OnTriggerEnter2D(Collider2D other)
    {
     // not working vvvv :/
         //plantText = GameObject.Find("PlantText");
         plantText.text = "Press H";
        Debug.Log("Object that entered the trigger : " + other);
+        isInTrigger = true;
+   }
+
+   void OnTriggerExit2D(Collider2D other) {
+    isInTrigger = false;
+   }
+
+   void Update() {
+        if (isInTrigger && Input.GetKeyDown("h")) {
+           Debug.Log("Harvesting...");
+            gameObject.SetActive(false);
+        }
+
    }
 }
