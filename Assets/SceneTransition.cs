4d9cf2b255ddde7e4fc2db5305304fb6788aab3c
Riddhi Bajaj <60859165+riddhi0610@users.noreply.github.com>
2023-02-05 14:25:54 -0500

    future to past

--- Assets/SceneTransition.cs
+++ Assets/SceneTransition.cs
@@ -6,11 +6,43 @@ using UnityEngine.SceneManagement;
 public class SceneTransition : MonoBehaviour
 {
     public string sceneToLoad;
+    public GameObject overlay;
+    public GameObject grass;
+    public GameObject overlaygray;
+    public GameObject grassgray;
+    public GameObject originalappletree;
+    public GameObject originalorangetree;
+    public GameObject goldenappletree;
+    public GameObject goldenorangetree;
+    public GameObject hourglass1;
+    public GameObject hourglass2;
+    private void Start(){
+        overlay.SetActive(true);
+        grass.SetActive(true);
+        overlaygray.SetActive(false);
+        grassgray.SetActive(false);
+        originalappletree.SetActive(true);
+        originalorangetree.SetActive(true);
+        goldenappletree.SetActive(false);
+        goldenorangetree.SetActive(false);
+        hourglass1.SetActive(true);
+        hourglass2.SetActive(true);
+    }
     public void OnTriggerEnter2D(Collider2D other){
         Debug.Log("test");
         if(other.gameObject.name == "Player"){
             Debug.Log("test");
-            SceneManager.LoadScene(sceneToLoad);
+            //SceneManager.LoadScene(sceneToLoad);
+            overlay.SetActive(false);
+            grass.SetActive(false);
+            overlaygray.SetActive(true);
+            grassgray.SetActive(true);
+            originalappletree.SetActive(false);
+            originalorangetree.SetActive(false);
+            goldenappletree.SetActive(true);
+            goldenorangetree.SetActive(true);
+            hourglass1.SetActive(false);
+            hourglass2.SetActive(false);
         }
     }
 }
